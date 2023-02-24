f9fda56c2c4b82e92102da342215e76a4fdd9aa0
Ria Das <riadas@mit.edu>
2023-02-24 15:57:48 -0500

    added cisc interface

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file, render_template, request, jsonify, send_from_directory
+from flask import Flask, send_file, render_template, request, jsonify, send_from_directory, redirect, url_for
 import json 
 from datetime import datetime
 import os 
@@ -7,7 +7,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+  return redirect(url_for('empa'))  # "<p>Hello, World!</p>"
+
+@app.route("/empa.html")
+def empa():
   return render_template("pedrotsividis.com/vgdl-games/games/gvgai_aliens/0.html")  # "<p>Hello, World!</p>"
+
+@app.route("/cisc.html")
+def cisc():
+  return render_template("pedrotsividis.com/vgdl-games/games/gvgai_aliens/index.html")  # "<p>Hello, World!</p>"
 
 @app.route('/<path:path>')
 def send_js(path):
@@ -35,7 +43,7 @@ def save(user_id):
 
   game_name = data["game_name"]
 
-  directory = "raj_traces/"+game_name
+  directory = "october_traces/"+game_name # new_traces
   if not os.path.isdir(directory):
     os.mkdir(directory)  
   with open(directory + "/TIME_" + str(datetime.now()) + ".json", 'w') as outfile:
